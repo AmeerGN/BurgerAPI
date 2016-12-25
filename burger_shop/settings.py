41f0361945916955455f341087330b1d1dd49083
Ameer Nasrallah <ameernasrallah@gmail.com>
2016-12-25 12:03:02 +0200

    Update settings.py to include rest_framework and burger_api app, and updated the time zone

--- burger_shop/settings.py
+++ burger_shop/settings.py
@@ -37,7 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'burger_api.apps.BurgerApiConfig',
 ]
+
+REST_FRAMEWORK = {
+    'PAGE_SIZE': 10
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -105,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Jerusalem'
 
 USE_I18N = True
 
